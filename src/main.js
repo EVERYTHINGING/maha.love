457b0791a5942352aba242905262ceee802f4ced
EVERYTHINGING <mikeparisi@gmail.com>
2022-02-15 14:35:38 -0500

    remove fps

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Grid from '@/components/Grid.vue'
 import Item from '@/components/Item.vue'
-import FPSMeter from 'fps-m';
+//import FPSMeter from 'fps-m';
 
+/*
 const fps = new FPSMeter({ui: true});
 fps.start();
 fps.element.classList.add('fps');
+*/
 
 const app = createApp(App);
 app.component("Grid", Grid);
