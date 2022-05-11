91fbabaac3e4de7818004c7c928e2c79aebf1a99
EVERYTHINGING <mikeparisi@gmail.com>
2022-05-11 17:50:19 -0400

    Update data.js

--- public/data.js
+++ public/data.js
@@ -3,112 +3,6 @@ window.treeData = {
     "name": "work",
     "children": [
         {
-            "path": "work/drawings",
-            "name": "drawings",
-            "children": [
-                {
-                    "path": "work/drawings/fire-of-spirit copy.jpg",
-                    "name": "fire-of-spirit copy.jpg"
-                },
-                {
-                    "path": "work/drawings/fire-of-spirit.jpg",
-                    "name": "fire-of-spirit.jpg"
-                },
-                {
-                    "path": "work/drawings/intelligences copy.jpg",
-                    "name": "intelligences copy.jpg"
-                },
-                {
-                    "path": "work/drawings/intelligences.jpg",
-                    "name": "intelligences.jpg"
-                },
-                {
-                    "path": "work/drawings/karmic-fields-of-creationpsd copy 2.jpg",
-                    "name": "karmic-fields-of-creationpsd copy 2.jpg"
-                },
-                {
-                    "path": "work/drawings/karmic-fields-of-creationpsd copy 3.jpg",
-                    "name": "karmic-fields-of-creationpsd copy 3.jpg"
-                },
-                {
-                    "path": "work/drawings/karmic-fields-of-creationpsd copy.jpg",
-                    "name": "karmic-fields-of-creationpsd copy.jpg"
-                },
-                {
-                    "path": "work/drawings/karmic-fields-of-creationpsd.jpg",
-                    "name": "karmic-fields-of-creationpsd.jpg"
-                },
-                {
-                    "path": "work/drawings/samsaric-unfurnelling copy 2.jpg",
-                    "name": "samsaric-unfurnelling copy 2.jpg"
-                },
-                {
-                    "path": "work/drawings/samsaric-unfurnelling copy 3.jpg",
-                    "name": "samsaric-unfurnelling copy 3.jpg"
-                },
-                {
-                    "path": "work/drawings/samsaric-unfurnelling copy.jpg",
-                    "name": "samsaric-unfurnelling copy.jpg"
-                },
-                {
-                    "path": "work/drawings/samsaric-unfurnelling.jpg",
-                    "name": "samsaric-unfurnelling.jpg"
-                },
-                {
-                    "path": "work/drawings/unfernnelling copy 2.jpg",
-                    "name": "unfernnelling copy 2.jpg"
-                },
-                {
-                    "path": "work/drawings/unfernnelling copy 3.jpg",
-                    "name": "unfernnelling copy 3.jpg"
-                },
-                {
-                    "path": "work/drawings/unfernnelling copy.jpg",
-                    "name": "unfernnelling copy.jpg"
-                },
-                {
-                    "path": "work/drawings/unfernnelling.jpg",
-                    "name": "unfernnelling.jpg"
-                },
-                {
-                    "path": "work/drawings/variations-of-the-one-1 copy 2.jpg",
-                    "name": "variations-of-the-one-1 copy 2.jpg"
-                },
-                {
-                    "path": "work/drawings/variations-of-the-one-1 copy 3.jpg",
-                    "name": "variations-of-the-one-1 copy 3.jpg"
-                },
-                {
-                    "path": "work/drawings/variations-of-the-one-1 copy.jpg",
-                    "name": "variations-of-the-one-1 copy.jpg"
-                },
-                {
-                    "path": "work/drawings/variations-of-the-one-1.jpg",
-                    "name": "variations-of-the-one-1.jpg"
-                },
-                {
-                    "path": "work/drawings/web-of-the-father copy 2.jpg",
-                    "name": "web-of-the-father copy 2.jpg"
-                },
-                {
-                    "path": "work/drawings/web-of-the-father copy 3.jpg",
-                    "name": "web-of-the-father copy 3.jpg"
-                },
-                {
-                    "path": "work/drawings/web-of-the-father copy.jpg",
-                    "name": "web-of-the-father copy.jpg"
-                },
-                {
-                    "path": "work/drawings/web-of-the-father.jpg",
-                    "name": "web-of-the-father.jpg"
-                }
-            ]
-        },
-        {
-            "path": "work/fire-of-spirit.jpg",
-            "name": "fire-of-spirit.jpg"
-        },
-        {
             "path": "work/illustration",
             "name": "illustration",
             "children": [
@@ -141,22 +35,6 @@ window.treeData = {
                     "name": "web-of-the-father.jpg"
                 }
             ]
-        },
-        {
-            "path": "work/intelligences.jpg",
-            "name": "intelligences.jpg"
-        },
-        {
-            "path": "work/karmic-fields-of-creationpsd.jpg",
-            "name": "karmic-fields-of-creationpsd.jpg"
-        },
-        {
-            "path": "work/samsaric-unfurnelling.jpg",
-            "name": "samsaric-unfurnelling.jpg"
-        },
-        {
-            "path": "work/unfernnelling.jpg",
-            "name": "unfernnelling.jpg"
         }
     ]
 }
